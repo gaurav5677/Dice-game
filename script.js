1da70bc59008797ebc7e01e94073ec73bcf78cf5
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-07 14:30:24 +0530

    dice is hidden now

---
+++ script.js
@@ -0,0 +1,8 @@
+// Selecting elements
+const score0L = document.querySelector("#score--0"); //Score0L element
+const score1 = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+score0L.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add("hidden");
