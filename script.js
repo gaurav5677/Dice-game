2db63ead8745fe9787e8be442f77b1ebdad94ba3
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-08 12:32:37 +0530

    Rolling the dice

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 const score0L = document.querySelector("#score--0"); //Score0L element
 const score1 = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
+///////////////////////////////////////////starting Condition/////////////////////////////
 score0L.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add("hidden");
+
+//////////////////////////////////////////////Rolling dice Functionality ///////////////////////////
+
+btnRoll.addEventListener("click", function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6 + 1);
+
+  // 2. display dice
+  diceEl.classList.remove("hidden");
+  // 3. check for rolle 1: if turn, switch to  next player
+});
